a77b29fe2606daf17429195116d235fedd66838a
Romi Petrelis <velcraft@outlook.com>
2015-02-13 17:12:52 -0600

    By default, wait for up to 5 seconds for the button to be clickable, in order to avoid "Element not clickable" exception

--- MK6.AutomatedTesting.UI/Components/Button.cs
+++ MK6.AutomatedTesting.UI/Components/Button.cs
@@ -1,16 +1,39 @@
-﻿using OpenQA.Selenium;
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace MK6.AutomatedTesting.UI.Components
 {
     public class Button : PageElement
     {
+        private readonly IWebDriver _browser;
+
         public Button(IWebDriver browser, By findBy)
             : base(browser, findBy)
-        { }
+        {
+            _browser = browser;
+        }
 
         public void Click()
         {
-            this.Element.Click();
+            Click(TimeSpan.FromSeconds(5));
+        }
+
+        public void Click(TimeSpan timeout)
+        {
+            var wait = new WebDriverWait(_browser, timeout);
+            wait.Until(driver =>
+            {
+                try
+                {
+                    Element.Click();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            });
         }
     }
 }
